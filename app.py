bf917e6af0600b520cf22a7e696f62faa1724f66
sskinner <sean.skinner@wsu.edu>
2022-10-14 14:15:28 -0700

    was able to successfully connect and run queries on database to retrieve desired results

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-from bottle import run, route
-import sqlite3
+from unittest import result
+from urllib.error import HTTPError
+import bottle
+import bottle.ext.sqlite
 
-@route('/')
+
+
+app = bottle.Bottle()
+plugin = bottle.ext.sqlite.Plugin(dbfile='Users.db')
+app.install(plugin)
+
+
+@app.route('/')
 def index():
     #Home Page to navigate the website
     print("On the home web page")
-    html =  """<h1>Home Page</h1>'
+    html =  """<h1>Home Page</h1>
             <a href='/SQL/Easy'>Easy SQL Inejction </a>"""
     # Give options for the user to navigate to new pages
     return html 
 
-@route('/SQL/<id>')
+@app.route('/SQL/<id>')
 def SQLEasy(id):
     print("On SQL Injection Page level " + id)
     html =  """<h1>Viewing the SQL Injection """+ id +""" Page </h1> 
@@ -18,10 +27,23 @@ def SQLEasy(id):
     #Return to the home page
     return html
 
+@app.route('/DBview')
+def DBview(db):
+    username = "Alex"
+    sql_statement = "SELECT * FROM Users WHERE User = '{username}'"
+    user = db.execute(sql_statement.format(username=username)).fetchone()
+    if(user):
+        name = user[0]
+        print("successfully pulled from database")
+        return "<h1> " + name + " </h1>"
+    return "<h1> Error </h1>"
+
+    
+
 # Old code when learning about routes
-#@route('/login')
+#@app.route('/login')
 #def login():
 #    return '<h1>Viewing Login Page<h1>'
 
 if __name__ == '__main__':
-    run(debug=True, reloader=True)
+    app.run(debug=True, reloader=True)
