53be0dca4d0a759d00f2c053466d6c8b842500c8
sskinner <sean.skinner@wsu.edu>
2022-10-14 12:31:11 -0700

    added more static and dyanmic routes

--- app.py
+++ app.py
@@ -4,5 +4,15 @@ from bottle import run, route
 def index():
     return '<h1>Hello World</h1>'
 
+@route('/login')
+def login():
+    return '<h1>Viewing Login Page<h1>'
+
+@route('/SQL/<id>')
+def SQLEasy(id):
+    return '<h1>Viewing the SQL Injection ' + id + ' Page </h1>'
+
+
+
 if __name__ == '__main__':
-    run()
+    run(debug=True, reloader=True)
